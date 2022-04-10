612f423f06903cf874b177ab017d83fdfb1a1a99
ijungyu <any3ok@naver.com>
2022-04-10 21:32:28 +0900

    namespace

--- socket.js
+++ socket.js
@@ -10,17 +10,29 @@ const init = (server) => {
       origin: "*",
     },
   });
-
-  io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
-    socket.on('disconnect', () => {
-    console.log('user disconnected');
-    });
-  });
+// NameSpace 1번
+const namespace1 = io.of('/namespace1');
+// connection을 받으면, news 이벤트에 hello 객체를 담아 보낸다
+namespace1.on('connection', (socket) => {
+  namespace1.emit('news', { hello: "Someone connected at namespace1" });
+});
+// NameSpace 2번
+const namespace2 = io.of('/namespace2');
+// connection을 받으면, news 이벤트에 hello 객체를 담아 보낸다
+namespace2.on('connection', (socket) => {
+  namespace2.emit('news', { hello: "Someone connected at Namespace2" });
+});
+  // io.on('connection', (socket) => {
+  //   console.log('a user connected');
+  //   socket.on('chat message', (msg) => {
+  //     io.emit('chat message', msg);
+  //   });
+  //   socket.on('disconnect', () => {
+  //   console.log('user disconnected');
+  //   });
+  // });
 }
+
 
 export default{
   init
